ce6b191dd1892a4e2b737c7c9aa9884c82d03ea6
LyudaShutova <141723673+LyudaShutova@users.noreply.github.com>
2023-08-28 12:09:39 +0300

    Commit

--- lift/src/Main.java
+++ lift/src/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        scanner.delimiter();
-        System.out.println("Введите n договоров и t минут");
         int n = scanner.nextInt();
         int t = scanner.nextInt();
         if (n <= 2 || n >= 100) {
@@ -16,8 +14,6 @@ public class Main {
         }
         int floors[] = new int[n];
         Scanner scanner1 = new Scanner(System.in);
-        scanner.delimiter();
-        System.out.println("Введите номера этажей");
         for (int i = 0; i < n; i++) {
             floors[i] = scanner1.nextInt();
             if (floors[i] > 100) {
@@ -28,7 +24,6 @@ public class Main {
         int lowFloor = floors[0];
         int lastFloor = floors[floors.length - 1];
         Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Введите индекс сотрудника t");
         int x = scanner2.nextInt();
         int numFloorT = 0;
         for (int i = 0; i < n; i++) {
